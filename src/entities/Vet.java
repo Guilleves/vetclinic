ffc2afb443870018378f9e4700d7fd67718b451c
guilleves <guilleves@gmail.com>
2016-06-30 11:27:04 -0300

    -Create-drop property to alter table

--- src/entities/Vet.java
+++ src/entities/Vet.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class Vet {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 	private Integer dni;
     private String name, address, birthday;
