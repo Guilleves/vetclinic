d19426f0addaaa4bf64a1ea6ad0fc9876e75eabe
guilleves <guilleves@gmail.com>
2016-07-04 16:21:28 -0300

    Add hashCode() and equals()

--- src/entities/Vet.java
+++ src/entities/Vet.java
@@ -30,18 +30,6 @@ public class Vet {
 		this.phone = phone;
 		this.birthday = birthday;
 	}
-	
-	//overriding original equals()
-	public boolean equals(Object obj){
-		boolean flag = false;
-		Vet vet = (Vet) obj;
-		if (this.dni == dni){
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
     
 	public String getName() {
 		return name;
@@ -85,5 +73,75 @@ public class Vet {
 	public String toString() {
 		return "Vet [dni=" + dni + ", name=" + name + ", address=" + address + ", license=" + license + ", phone="
 				+ phone + ", birthday=" + birthday + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((birthday == null) ? 0 : birthday.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + ((license == null) ? 0 : license.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Vet other = (Vet) obj;
+		if (address == null) {
+			if (other.address != null) {
+				return false;
+			}
+		} else if (!address.equals(other.address)) {
+			return false;
+		}
+		if (birthday == null) {
+			if (other.birthday != null) {
+				return false;
+			}
+		} else if (!birthday.equals(other.birthday)) {
+			return false;
+		}
+		if (dni == null) {
+			if (other.dni != null) {
+				return false;
+			}
+		} else if (!dni.equals(other.dni)) {
+			return false;
+		}
+		if (license == null) {
+			if (other.license != null) {
+				return false;
+			}
+		} else if (!license.equals(other.license)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (phone == null) {
+			if (other.phone != null) {
+				return false;
+			}
+		} else if (!phone.equals(other.phone)) {
+			return false;
+		}
+		return true;
 	}
 }
